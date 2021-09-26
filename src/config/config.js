ca0e1b927eac63fb654c48e9b1a2f2e6e849abff
Mukul <mukulkolpe45@gmail.com>
2021-09-27 03:46:53 +0530

    initial commit

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4JA2hlUa5caQ7sIkLnYp3GbqZ1Q6fohE",
+    authDomain: "todo-list-app-145f5.firebaseapp.com",
+    projectId: "todo-list-app-145f5",
+    storageBucket: "todo-list-app-145f5.appspot.com",
+    messagingSenderId: "41279068594",
+    appId: "1:41279068594:web:197817199ff072d552acab"
+  };
